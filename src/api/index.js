9aff2e7d140fccefb0e8f2ab35a209d0cf2bf1d6
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-09-01 21:59:25 +0800

    remove token

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+const token = null
+
 const api = {
-    getWeather: ({city, country}) => axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=5de7e1d7b2299d7f228691833a9d8358&lang=nl`)
+    getWeather: ({ city, country }) =>
+        axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${token}&lang=nl`
+        )
 }
 
 export default api
